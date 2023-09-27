a4f5f7b2302b11f0c42fb8fb0f369a24f2193597
ajeet p <ajeetpawar07@gmail.com>
2023-09-27 11:58:22 +0530

    render deploy v1.3

--- server.js
+++ server.js
@@ -1,30 +1,10 @@
-const jsonServer = require("json-server");
-const express = require("express");
-const path = require("path");
-
+const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
-
-// Serve images statically from the 'images' directory
-server.use("/images", express.static(path.join(__dirname, "images")));
-
-server.use((req, res, next) => {
-  // Modify image paths to use the "/images" route
-  if (req.method === "GET" && req.path === "/products") {
-    const products = router.db.get("products").value();
-    products.forEach((product) => {
-      product.images = product.images.map((image) => `/images${image}`);
-    });
-    res.jsonp({ products });
-  } else {
-    next();
-  }
-});
-
 server.use(router);
 
 server.listen(port);
